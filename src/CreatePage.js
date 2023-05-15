a60fd18e60e0a11c8362edb7f0d536b66bf60fc2
wtucker <wtbtucker@gmail.com>
2023-05-15 16:38:40 -0400

    Create dynamic form for creating recipes. This form includes a buttons for adding an indredient and adding a step to the instructions.

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,27 +1,92 @@
 import Recipe from "./Recipe";
 import { useState } from 'react';
+import uniqid from 'uniqid';
 
-// pass recipes array into the create recipe component as state
-// update the state when recipe form is submitted and pass the new array up to home
-// render the updated array
-
-// Pass existing recipe object into the form if editing the recipe
+const RecipeForm = ({ handleSubmit }) => {
 
 
-const RecipeForm = ({ handleSubmit, handleChange }) => {
+    const recipeTemplate = {
+        title: '',
+        ingredients: [{ ingredient_name: ''}],
+        instructions: [{ instruction_step: ''}]
+    }
+
+    const [responseBody, setResponseBody] = useState(recipeTemplate);
+
+    const handleChange = event => {
+        if (event.target.name === 'ingredient_name') {
+            let ingredients = [...responseBody.ingredients];
+            ingredients[event.target.id][event.target.name] = event.target.value;
+            setResponseBody({ ingredients })
+        } 
+        else if (event.target.name === 'instruction_step') {
+            let instructions = [...responseBody.instructions];
+            instructions[event.target.id][event.target.name] = event.target.value
+            setResponseBody({ instructions })
+        } 
+        else {
+            setResponseBody({ ...responseBody, [event.target.name]: event.target.value})
+        }
+    };
+
+
+    const addIngredient = event => {
+        event.preventDefault();
+        setResponseBody(prevState => ({
+            ...prevState,
+            ingredients: [...prevState.ingredients, {ingredient_name: ''}]
+        }));
+    };
+
+    const addStep = event => {
+        event.preventDefault();
+        setResponseBody(prevState => ({
+            ...prevState,
+            instructions: [...prevState.instructions, {instruction_step: ''}]
+        }));
+    }
+
+    let ingredient_list = responseBody.ingredients?.map((ingredient, index) => {
+        return (
+            <div key={index}>
+            <input 
+                type="text"
+                className="form-control"
+                id={index}
+                placeholder="Ingredient Name"
+                name="ingredient_name"
+            />
+        </div>
+        );
+    });
+
+    let instruction_steps = responseBody.instructions?.map((step, index) => {
+        return (
+            <div key={index}>
+                <input 
+                type="text"
+                className="form-control"
+                id={index}
+                placeholder="Step"
+                name="instruction_step"
+            />
+            </div>
+        )
+    })
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} onChange={handleChange}>
             <div className="mb-3">
                 <label htmlFor="inputTitle" className="form-label">Title</label>
-                <input type="text" className="form-control" id="inputTitle" name="title" onChange={e=>handleChange(e)}></input>
+                <input type="text" className="form-control" id="inputTitle" name="title"></input>
             </div>
             <div className="mb-3">
-                <label htmlFor="inputIngredients" className="form-label">Ingredients</label>
-                <input type="text" className="form-control" id="inputIngredients" name="ingredients" onChange={e=>handleChange(e)}></input>
+                {ingredient_list}
+                <button className="btn btn-secondary" onClick={addIngredient}>Add Ingredient</button>
             </div>
             <div className="mb-3">
-                <label htmlFor="inputInstructions" className="form-label">Instructions</label>
-                <input type="text" className="form-control" id="inputInstructions" name="instructions" onChange={e=>handleChange(e)}></input>
+                {instruction_steps}
+                <button onClick={addStep} className="btn btn-secondary">Add Step</button>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
@@ -29,14 +94,7 @@ const RecipeForm = ({ handleSubmit, handleChange }) => {
 }
 
 const CreatePage = () => {
-
-    const recipeTemplate = {
-        title: '',
-        ingredients: [],
-        instructions: []
-    }
-
-    const [responseBody, setResponseBody] = useState(recipeTemplate)
+    
     const [recipes, setRecipes] = useState([])
 
     const handleSubmit = (event) => {
@@ -44,26 +102,25 @@ const CreatePage = () => {
         const formData = new FormData(event.target);
         const recipe = Object.fromEntries(formData.entries());
         console.log(recipe)
+        recipe.id = uniqid();
         setRecipes(prevState => ({ recipes: [...prevState, recipe]}));
         console.log(recipes)
-    }
+    };
 
-    const handleChange = (event) => {
-        const {name, value} = event.target
-        setResponseBody({...responseBody, [name]: value})
-    }
 
     let content;
     if (recipes.length === 0) {
-        content = <RecipeForm handleSubmit={handleSubmit} handleChange = {handleChange}/>
+        content = <RecipeForm handleSubmit={handleSubmit}/>
     } else {
         console.log(recipes)
-        content = recipes.recipes?.map(recipe => <Recipe recipe={recipe}/>)
+        content = recipes.recipes?.map(recipe => <li key={recipe.id} className="list-group-item"><Recipe recipe={recipe}/></li>)
     }
 
     return (
         <div>
-            {content}            
+            <ul className="list-group">
+                {content}
+            </ul>          
         </div>
         
     )
